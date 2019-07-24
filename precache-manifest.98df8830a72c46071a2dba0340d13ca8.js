e90d2c3be1b0fe029f1045857c620f73c1072ac0
prayuj <prayuj@gmail.com>
2019-07-24 17:03:52 +0530

    Updates

---
+++ precache-manifest.98df8830a72c46071a2dba0340d13ca8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d225d4f07675a7fae1ee6ef8dee57257",
+    "url": "/Bill-Spliter/index.html"
+  },
+  {
+    "revision": "09debb8a81677397d82b",
+    "url": "/Bill-Spliter/static/css/2.b49442d9.chunk.css"
+  },
+  {
+    "revision": "ae56a534efc5914d08eb",
+    "url": "/Bill-Spliter/static/css/main.771cc898.chunk.css"
+  },
+  {
+    "revision": "09debb8a81677397d82b",
+    "url": "/Bill-Spliter/static/js/2.291312c5.chunk.js"
+  },
+  {
+    "revision": "ae56a534efc5914d08eb",
+    "url": "/Bill-Spliter/static/js/main.ae9f10d4.chunk.js"
+  },
+  {
+    "revision": "a725ae91837666bf90da",
+    "url": "/Bill-Spliter/static/js/runtime~main.1b266df3.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Bill-Spliter/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Bill-Spliter/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Bill-Spliter/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Bill-Spliter/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Bill-Spliter/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
